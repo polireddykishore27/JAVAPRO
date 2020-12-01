5cc0f00cc19f447b47926990e31e442829d1b611
polireddykishore27 <67549664+polireddykishore27@users.noreply.github.com>
2020-12-01 23:12:02 +0530

    Add files via upload
    Java Program For Add 2 numbers

---
+++ Addnumbers.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Addnumbers {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;
+		int sum=a+b;
+		System.out.println("the total Sum "+sum);
+
+	}
+
+}
